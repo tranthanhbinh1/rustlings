3b5af688ad5e07cef892b5dd5c03ec22a67936cb
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2024-04-09 22:19:24 +0700

    did some more exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apple: i32) -> i32 {
+    if num_apple <= 40 {
+        num_apple * 2
+    }
+    else {
+        num_apple * 1
+    }
+}
 
 // Don't modify this function!
 #[test]
